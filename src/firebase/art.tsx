9ee7be3500c14428241b2653cfad5cdc4b5925bb
Nuccino92 <anthnucci@hotmail.com>
2022-05-03 00:24:00 -0400

    database query file

--- src/firebase/art.tsx
+++ src/firebase/art.tsx
@@ -4,15 +4,13 @@ import { getDocs, orderBy, query, collection } from "firebase/firestore";
 // collection ref
 const colRef = collection(database, "art");
 
-console.log(colRef);
-
 const q = query(colRef, orderBy("author", "asc"));
 
 // get collection data
 export const art = () =>
   getDocs(q)
     .then((snapshot) => {
-      let art = [];
+      let art: Array<Object> = [];
       snapshot.docs.forEach((doc) => {
         art.push({ ...doc.data(), id: doc.id });
       });
