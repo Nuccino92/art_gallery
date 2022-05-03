384576f5f0487ad81dbda7a25e4f75b6730856cf
Nuccino92 <anthnucci@hotmail.com>
2022-05-03 00:24:09 -0400

    updated for sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import "./App.css";
+import { useEffect } from "react";
+import "./App.scss";
+import { art } from "./firebase/art";
 
 function App() {
+  useEffect(() => {
+    art().then((res) => {
+      console.log(res);
+    });
+  }, []);
   return <div className="App"></div>;
 }
 
