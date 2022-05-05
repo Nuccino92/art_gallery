839080fed33c1516aab7bb3f7575ee4ee24fc33c
Nuccino92 <anthnucci@hotmail.com>
2022-05-04 23:29:18 -0400

    added sidenav to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Artwork from "./pages/artwork/Artwork";
 import NotFound from "./pages/notFound/NotFound";
+import SideNav from "./components/sidenav/SideNav";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="App">
+        <SideNav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/art/:id" element={<Artwork />} />
