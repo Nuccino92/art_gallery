746a85fcc26c97509aaa36fd28abe18da441f69d
Nuccino92 <anthnucci@hotmail.com>
2022-05-05 22:20:27 -0400

    homepage header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from "react";
 import "./Header.scss";
+import UnsplashLogo from "./unsplash-logo.png";
+import { Nav, Navbar, Image } from "react-bootstrap";
 
 const Header: React.FC = () => {
   const headerRef = useRef<HTMLDivElement>(null);
@@ -10,9 +12,26 @@ const Header: React.FC = () => {
     });
   }, []);
   return (
-    <div ref={headerRef} className="Header">
-      heady
-    </div>
+    <Navbar
+      ref={headerRef}
+      className="Header d-flex justify-content-center sticky-top"
+      style={{
+        height: "22vh",
+        transition: "0.8s ease",
+        backgroundColor: "rgba(255, 255, 255, 0.676)",
+      }}
+    >
+      <Nav.Link href="https://unsplash.com/" target="_blank" className="h-100">
+        <Image
+          src={UnsplashLogo}
+          fluid
+          className="h-100 w-100 "
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </Nav.Link>
+    </Navbar>
   );
 };
 
