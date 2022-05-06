1e2c4d1756b3c2ecfe7546a7d14dacf99cc54088
Nuccino92 <anthnucci@hotmail.com>
2022-05-05 22:19:50 -0400

    added component that displays the authors art

--- src/pages/artwork/Artwork.tsx
+++ src/pages/artwork/Artwork.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import ArtDisplay from "../../components/artDisplay/ArtDisplay";
+import { art } from "../../firebase/art";
+import "./Artwork.scss";
+import { Art } from "../../models/art";
+
+// component displaying all of the authors work
 const Artwork: React.FC = () => {
-  return <div className="ArtWork">ART</div>;
+  const [artData, setArtData] = useState<Array<Art>>([]);
+
+  useEffect(() => {
+    art().then((res) => {
+      // retrieve firebase data and shuffle to display different photos each time
+      const shuffle = [].concat(res);
+      shuffle.sort(() => {
+        return 0.5 - Math.random();
+      });
+      setArtData(shuffle);
+    });
+  }, []);
+  return (
+    <Container className="ArtWork">
+      {" "}
+      <ArtDisplay artData={artData} />
+    </Container>
+  );
 };
 
 export default Artwork;
