c41b6bcfd4c0ad178bba4825956c9504d7979768
Nuccino92 <anthnucci@hotmail.com>
2022-05-06 22:32:41 -0400

    info component

---
+++ src/pages/info/Info.tsx
@@ -0,0 +1,8 @@
+import "./Info.scss";
+import { Container } from "react-bootstrap";
+
+const Info: React.FC = () => {
+  return <Container className="Info">infodfssdffdsfsdfsdsfd</Container>;
+};
+
+export default Info;
