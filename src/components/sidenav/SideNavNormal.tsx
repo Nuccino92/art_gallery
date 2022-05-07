c00a1fb46b89a9ffc1dad638c99703664bd1ad55
Nuccino92 <anthnucci@hotmail.com>
2022-05-07 19:29:09 -0400

    added search bar functionality

--- src/components/sidenav/SideNavNormal.tsx
+++ src/components/sidenav/SideNavNormal.tsx
@@ -1,20 +1,49 @@
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import { Navbar, InputGroup, Button, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { ArtContext } from "../context/artContext";
+import { Art } from "../../models/art";
 
 const SideNavNormal = () => {
+  const artContext = useContext(ArtContext);
+
+  // state for searchbar data
+  const [searchFilter, setSearchFilter] = useState<Array<Art> | void>([]);
+
+  // artContext?.allArt
+
+  const handleFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    const word = e.target.value;
+
+    const removeDuplicateAuthors = artContext?.allArt.filter(
+      (v, i, a) => a.findIndex((v2) => v.author === v2.author) === i
+    );
+
+    const newFilter = removeDuplicateAuthors?.filter((art) => {
+      return art.author.toLowerCase().includes(word.toLowerCase());
+    });
+
+    word === ""
+      ? setSearchFilter(artContext?.allArt)
+      : setSearchFilter(newFilter);
+  };
+
+  useEffect(() => {
+    console.log(searchFilter);
+  }, [searchFilter]);
+
   return (
-    <Navbar
-      className="SideNavNormal"
-      style={{ color: "black", zIndex: "100000" }}
-    >
+    <Navbar className="SideNavNormal" style={{ color: "black" }}>
       <Link className="SideNav-header" to="/">
         Photo Gallery
       </Link>
-
       <InputGroup>
-        <FormControl aria-label="Search" />
+        <FormControl aria-label="Search" onChange={handleFilter} />
         <Button>Search</Button>
       </InputGroup>
+      <Link className="SideNav-header" to="/">
+        Home
+      </Link>
       <Link to="/info">Description</Link>
     </Navbar>
   );
--- src/components/sidenav/SideNavSmall.tsx
+++ src/components/sidenav/SideNavSmall.tsx
@@ -7,11 +7,38 @@ import {
   FormControl,
   Container,
 } from "react-bootstrap";
-import { useState } from "react";
+import { useContext, useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
+import { ArtContext } from "../context/artContext";
+import { Art } from "../../models/art";
 
 const SideNavSmall = () => {
   const [opened, setOpened] = useState<boolean>(false);
+  const artContext = useContext(ArtContext);
+
+  const [searchFilter, setSearchFilter] = useState<Array<Art> | void>([]);
+
+  // artContext?.allArt
+
+  const handleFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    const word = e.target.value;
+
+    const removeDuplicateAuthors = artContext?.allArt.filter(
+      (v, i, a) => a.findIndex((v2) => v.author === v2.author) === i
+    );
+
+    const newFilter = removeDuplicateAuthors?.filter((art) => {
+      return art.author.toLowerCase().includes(word.toLowerCase());
+    });
+
+    word === ""
+      ? setSearchFilter(artContext?.allArt)
+      : setSearchFilter(newFilter);
+  };
+
+  useEffect(() => {
+    console.log(searchFilter);
+  }, [searchFilter]);
 
   return (
     <>
@@ -24,7 +51,7 @@ const SideNavSmall = () => {
             </Link>
 
             <InputGroup>
-              <FormControl aria-label="Search" />
+              <FormControl aria-label="Search" onChange={handleFilter} />
               <Button>Search</Button>
             </InputGroup>
             <Link to="/info">Description</Link>
@@ -33,7 +60,7 @@ const SideNavSmall = () => {
       ) : (
         <Container className="hamburger-container">
           <GiHamburgerMenu
-            size={55}
+            size={47}
             onClick={() => setOpened(true)}
             style={{ overflow: "visible" }}
           />
