cb86bdef362fbaeab10aefb211995d709ca7fc40
Nuccino92 <anthnucci@hotmail.com>
2022-05-08 23:24:44 -0400

    implemented context state

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,34 @@
-import { useEffect, useState } from "react";
-import { art } from "../../firebase/art";
+import { useContext, useEffect } from "react";
 import ArtDisplay from "../../components/artDisplay/ArtDisplay";
-import { Art } from "../../models/art";
+import { ArtContext } from "../../components/context/artContext";
 import Header from "../../components/header/Header";
+import { art as artQuery } from "../../firebase/art";
 import "./Home.scss";
 
 const Home: React.FC = () => {
-  const [artData, setArtData] = useState<Array<Art>>([]);
+  const artContext = useContext(ArtContext);
 
   useEffect(() => {
-    art().then((res) => {
-      // retrieve firebase data and shuffle to display different photos each time
+    artQuery().then((res) => {
+      // keeps search bar functionality throughout the app
+      artContext?.setAllArt(res);
+
+      // shuffling the response to randomize the display of images
       const shuffle = [].concat(res);
       shuffle.sort(() => {
         return 0.5 - Math.random();
       });
-      setArtData(shuffle);
+      // setting the art state in context to the shuffled array
+      artContext?.setArt(shuffle);
     });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="Home">
-      <Header />
-      <ArtDisplay artData={artData} />
+      <Header location={"home"} author={undefined} link={undefined} />
+      <ArtDisplay />
     </div>
   );
 };
