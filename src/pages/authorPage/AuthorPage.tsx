25a63e6136535a7d673d1e5d5cd80c5fb905a364
Nuccino92 <anthnucci@hotmail.com>
2022-05-08 23:25:13 -0400

    fixed query

--- src/pages/authorPage/AuthorPage.tsx
+++ src/pages/authorPage/AuthorPage.tsx
@@ -13,29 +13,36 @@ import { ArtContext } from "../../components/context/artContext";
 const AuthorPage: React.FC = () => {
   const artContext = useContext(ArtContext);
 
+  const [nameObject, setnameObject] = useState<Art | void>();
+
   const { id: author } = useParams();
 
   useEffect(() => {
+    const arr: Array<Art> = [];
+
     art().then((res) => {
-      artContext?.setArt([]);
       // retrieve firebase data and shuffle to display different photos each time
       res.map((e: Art) => {
         if (e.author.toLowerCase() === author?.toLowerCase()) {
-          return artContext?.setArt((prev) => [...prev, e]);
+          arr.push(e);
         }
+        return artContext?.setArt(arr);
       });
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [author]);
+
+  useEffect(() => {
+    artContext && setnameObject(artContext?.art[0]);
+  }, [artContext, artContext?.art]);
 
   return (
     <Container className="AuthorPage">
       <Header
         location={"author"}
         author={author}
-        link={"https://unsplash.com/"}
+        link={`${nameObject?.profile}`}
       ></Header>
-
       <ArtDisplay />
     </Container>
   );
