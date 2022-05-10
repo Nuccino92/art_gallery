0471b1cdf63e3764dbe4a9a13284176683c71df1
Nuccino92 <anthnucci@hotmail.com>
2022-05-09 20:42:18 -0400

    finished not found component

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,5 +1,18 @@
+import { Container } from "react-bootstrap";
+
 const NotFound: React.FC = () => {
-  return <div className="NotFound">404</div>;
+  return (
+    <Container
+      style={{
+        color: "black",
+        fontSize: "100px",
+        margin: "0 auto",
+      }}
+      className="NotFound"
+    >
+      404 Not found
+    </Container>
+  );
 };
 
 export default NotFound;
